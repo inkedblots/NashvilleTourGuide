e5b4813f6aa5041b9a5869513338625898c1962e
Michele <inkedblots@hotmail.com>
2018-07-05 12:28:24 -0700

    misc. edits

--- app/src/main/java/com/example/android/NashvilleTourGuide/LocationDetailActivity.java
+++ app/src/main/java/com/example/android/NashvilleTourGuide/LocationDetailActivity.java
@@ -1,17 +1,39 @@
 package com.example.android.NashvilleTourGuide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.TextView;
 
 public class LocationDetailActivity extends AppCompatActivity {
+
+    int locations;
+    TextView musicTitle;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_detail);
 
+        // Name of location
+        findViewById(R.id.titleTextView);
 
+        //get the current intent
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            locations = bundle.getInt("musicTitle");
+
+
+            String locationTitle = locations.get(position).getTitle();
+            String locationAddress = locations.get(position).getAddress();
+            String locationHours = locations.get(position).getBusinessHours();
+            String locationPrice = locations.get(position).getPrice();
+            String locationDescription = locations.get(position).getDescription();
+            int locationResource = locations.get(position).getImageResourceId();
+
+        }
     }
 }
--- app/src/main/java/com/example/android/NashvilleTourGuide/MusicFragment.java
+++ app/src/main/java/com/example/android/NashvilleTourGuide/MusicFragment.java
@@ -16,8 +16,15 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class MusicFragment extends Fragment {
 
+    String title;
+    String address;
+    String businessHours;
+    String entrancePrice;
+    String description;
+    int imageResourceId;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -25,48 +32,46 @@ public class MusicFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.places_list, container, false);
 
         //Create a list of music
-
         final ArrayList<Location> locations = new ArrayList<>();
-
         //locations.add("one);
-
         locations.add(new Location(getString(R.string.musicTitle), getString(R.string.musicAddress),
                 getString(R.string.musicHours), getString(R.string.musicPrice),
                 getString(R.string.musicDescription), R.drawable.tootsies));
-
         locations.add(new Location(getString(R.string.musicTitle2), getString(R.string.musicAddress2),
                 getString(R.string.musicHours2), getString(R.string.musicPrice2),
                 getString(R.string.musicDescription2), R.drawable.ryman));
-
         locations.add(new Location(getString(R.string.musicTitle3), getString(R.string.musicAddress3),
                 getString(R.string.musicHours3), getString(R.string.musicPrice3),
                 getString(R.string.musicDescription3), R.drawable.wildhors));
-
         locations.add(new Location(getString(R.string.musicTitle4), getString(R.string.musicAddress4),
                 getString(R.string.musicHours4), getString(R.string.musicPrice4),
                 getString(R.string.musicDescription4), R.drawable.walkoffame));
 
-        // This list item layout contains a layout of location information, that includes
-        // title, address, business hours, description and photo of the location which the adapter will
-        // set to display.
         LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
-        // Find the {@Link ListView} object in the view hierarchy of the {@Link Activity}.
-        // A {@Link ListView} with the view ID called list is declared in the places_list.xml.
         ListView listView = rootView.findViewById(R.id.list);
-        // Make the {@link ListView} use the {@link ArrayAdapter} created above, so that the
-        // {@link ListView} will display list items for each location in the list of places.
-        // Call the setAdapter method on the {@link ListView} object and pass in
-        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
         listView.setAdapter(adapter);
 
         // Set a click listener to select the location when the card is clicked on.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Location currentSelection = locations.get(position); //Creates a unique identifier for the item you selected
+                title = currentSelection.getTitle();
+                address = currentSelection.getAddress();
+                businessHours = currentSelection.getBusinessHours();
+                entrancePrice = currentSelection.getPrice();
+                description = currentSelection.getDescription();
+                imageResourceId = currentSelection.getImageResourceId();
 
-                // Send intent to new {@Link LocationDetailActivity} with details for the current selected
-                // location.
-                Intent intent = new Intent(getActivity(), LocationDetailActivity.class);
+                Intent intent = new Intent(view.getContext(), LocationDetailActivity.class);
+
+                String musicTitle = locations.get(position).getTitle();
+                intent.putExtra("title", musicTitle);
+                intent.putExtra("address", locationAddress);
+                intent.putExtra("hours", locationHours);
+                intent.putExtra("price", locationPrice);
+                intent.putExtra("description", locationDescription);
+                intent.putInt("image", locationResource);
                 startActivity(intent);
 
             }
@@ -74,6 +79,4 @@ public class MusicFragment extends Fragment {
 
         return rootView;
     }
-
-
 }
